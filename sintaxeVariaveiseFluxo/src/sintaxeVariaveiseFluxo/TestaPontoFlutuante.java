56cc9eb8d07ce2f68c2d30e78fdb5a3e8cb49280
glauber <glauber@pc>
2020-03-11 11:09:37 -0300

    aprendendo int e double

---
+++ sintaxeVariaveiseFluxo/src/sintaxeVariaveiseFluxo/TestaPontoFlutuante.java
@@ -0,0 +1,19 @@
+package sintaxeVariaveiseFluxo;
+
+public class TestaPontoFlutuante {
+	
+	public static void main(String [] args) {
+		double salario;
+		salario = 1250.70;
+		System.out.println("Meu salário é "+ salario);
+	
+		double idade = 37;
+		
+		double divisao = 3.14 / 2;
+		System.out.println("A divisão é "+divisao);
+		
+		int outraDivisao = 5 / 2;
+		System.out.println("Inteiro dividido é inteiro, 5 / 2 é: "+outraDivisao);
+	}
+
+}
